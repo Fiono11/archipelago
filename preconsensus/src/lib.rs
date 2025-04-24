ca373a5c3314b3e50a6a9df882dbabb99f90d24c
Fiono <ruipedromorais11@gmail.com>
2025-04-24 14:00:20 +0100

    Initial preconsensus tests
    lastFile:preconsensus/target/.rustc_info.json
    
    mob next [ci-skip] [ci skip] [skip ci]
    
    lastFile:consensus/target/debug/.fingerprint/arquipelago-c6a5ca800faea787/test-lib-arquipelago.json
    
    Fix

---
+++ preconsensus/src/lib.rs
@@ -0,0 +1,67 @@
+use std::hash::Hash;
+
+use rsnano_core::{BlockHash, BlockHashBuilder};
+
+const FRONTIERS_THRESHOLD: usize = 1000;
+
+// For a preproposal to be valid:
+// - The length must be equal to FRONTIERS_THRESHOLD
+// - It must contain only valid final voted blocks
+// - The preproposal needs to be reliably broadcast, i.e, a preproposal needs to be echoed by at least 2f+1 nodes
+#[derive(Clone)]
+struct PreProposal {
+    frontiers: Vec<BlockHash>
+}
+
+impl PreProposal {
+    fn hash(&self) -> BlockHash {
+        let mut hasher =     BlockHashBuilder::new();
+        for frontier in &self.frontiers {
+            hasher = hasher.update(frontier.as_bytes());
+        }
+        hasher.build()
+    }
+}
+
+// Fork of A and B
+// Node 1 preproposes A because it has final voted it
+// Node 2 preproposes A because it has final voted it
+// Node 3 
+// Node 4 (byzantine) preproposes B because it is byzantine
+
+struct Proposal {
+    // 2f+1 valid preproposals hashes
+    // The proposal must contain all the frontiers that are included in at least f+1 preproposals
+    preproposals: Vec<BlockHash>
+}
+
+// Goal: Guarantee that every valid proposal contains all the blocks that were committed (final voted) by at least one correct node (f+1)
+// This is because a block to be confirmed needs at least 2f+1 final votes
+// So if a given block only has f final votes out of 2f+1 preproposals, it means it wasn't confirmed
+
+fn create_proposal(preproposals: Vec<PreProposal>) -> Proposal {
+    Proposal {
+        preproposals: preproposals.iter().map(|p| p.hash()).collect()
+    }
+}
+
+#[test]
+fn preproposal_to_proposal() {
+    let preproposal = PreProposal {
+        frontiers: vec![BlockHash::from(1)]
+    };
+    
+    let hash = preproposal.hash();
+    let proposal = create_proposal(vec![preproposal]);
+
+    assert_eq!(proposal.preproposals, vec![hash]);
+}
+
+#[test]
+fn preproposal_hash() {
+    let preproposal = PreProposal {
+        frontiers: vec![BlockHash::from(1)]
+    };
+
+    assert_eq!(preproposal.hash(), BlockHash::decode_hex("33E423980C9B37D048BD5FADBD4A2AEB95146922045405ACCC2F468D0EF96988").unwrap());
+}
